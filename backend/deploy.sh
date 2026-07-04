ea8fa35d16774d521ada3e548393cddc83959080
tadjela <essolisamtadjela@gmail.com>
2026-07-04 10:02:17 +0000

    Force: Fresh migration and seeding on start

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# 1. On force la réinitialisation de la BDD et le seed immédiatement
+# Désactiver temporairement le cache pour éviter les conflits
+php artisan config:clear
+php artisan route:clear
+
+# Forcer la suppression de toutes les tables existantes et réexécuter le seeder
 php artisan migrate:fresh --seed --force
 
-# 2. On met en cache (optionnel)
-php artisan config:cache
-php artisan route:cache
-
-# 3. Lancement officiel du serveur requis par Render
+# Lancement officiel du serveur requis par Render
 php artisan serve --host=0.0.0.0 --port=10000
