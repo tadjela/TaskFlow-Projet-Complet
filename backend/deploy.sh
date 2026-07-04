2afc1a0516e030b8d531e3be79a60e5ded705ef6
tadjela <essolisamtadjela@gmail.com>
2026-07-04 10:08:29 +0000

    Force: Fresh migration and seeding on start

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,6 +6,6 @@ php artisan route:clear
 
 # Forcer la suppression de toutes les tables existantes et réexécuter le seeder
 php artisan migrate:fresh --seed --force
-
+#
 # Lancement officiel du serveur requis par Render
 php artisan serve --host=0.0.0.0 --port=10000
