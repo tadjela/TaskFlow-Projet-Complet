109343efb97c2eec107711f175458a182fd9b35b
tadjela <essolisamtadjela@gmail.com>
2026-07-04 10:17:13 +0000

    Log: verifier les erreurs de migration

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Désactiver temporairement le cache pour éviter les conflits
-php artisan config:clear
-php artisan route:clear
+echo "=== DEBUT DU SCRIPT DE DEPLOIEMENT ==="
 
-# Forcer la suppression de toutes les tables existantes et réexécuter le seeder
-php artisan migrate:fresh --seed --force
-#
+# On lance la migration et on affiche l'erreur s'il y en a une
+php artisan migrate:fresh --seed --force || echo "ATTENTION : Echec de la migration ou du seed !"
+
+echo "=== FIN DES MIGRATIONS - DEMARRAGE DU SERVEUR ==="
+
 # Lancement officiel du serveur requis par Render
 php artisan serve --host=0.0.0.0 --port=10000
